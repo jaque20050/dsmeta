8d322e0e7bb5884dd67d5673ba892c504734b174
Jaqueline Camilo Dias <jaque20050@hotmail.com>
2022-07-12 13:57:48 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Ola Mundo</h1>
+    <>
+      <h1>Ola Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
